cb61700400520b1477b6615b10841ca6606ba180
zarav <zarkov.av@bratsk-city.ru>
2019-05-28 11:08:05 +0800

    Смена bootstrap с 3 версии на версию 4.

---
+++ RegistryWeb/ViewComponents/OwnerReasonComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RegistryWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistryWeb.ViewComponents
+{
+    public class OwnerReasonComponent : ViewComponent
+    {
+        private RegistryContext registryContext;
+
+        public OwnerReasonComponent(RegistryContext registryContext)
+        {
+            this.registryContext = registryContext;
+        }
+
+        public IViewComponentResult Invoke(int id = 0)
+        {
+            ViewBag.Id = id;
+            return View("OwnerReason", registryContext.OwnerReasonTypes);
+        }
+    }
+}
---
+++ RegistryWeb/wwwroot/js/ownerProcess.create.js
@@ -0,0 +1,16 @@
+﻿$(function () {
+    $('#addressAdd').on("click", function () {
+        var id = $('.addressBlock').length;
+        $.ajax({
+            type: 'POST',
+            url: window.location.origin + '/OwnerProcesses/AddressAdd',
+            data: { id: id },
+            success: function (data) {
+                $('#addresses').append(data);
+                var newAddress = $('.addressBlock').last();
+                typeAddressRadiobuttonChange(newAddress);
+                autocompleteStreet(newAddress);
+            }
+        });
+    });
+});
