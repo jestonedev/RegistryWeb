080ad56f0e583068c389bd5b682f8cd2e45b6b21
zarav <zarkov.av@bratsk-city.ru>
2019-11-18 10:40:21 +0800

    Начало работы над выводом статистики по отчету

---
+++ RegistryWeb/Controllers/StatisticController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RegistryWeb.Models;
+using RegistryWeb.ViewModel;
+
+namespace RegistryWeb.Controllers
+{
+    [Authorize]
+    public class StatisticController : Controller
+    {
+        private RegistryContext registryContext;
+
+        public StatisticController(RegistryContext registryContext)
+        {
+            this.registryContext = registryContext;
+        }
+
+        [HttpPost]
+        public JsonResult ReestrStatistic()
+        {
+            var numbers = new int[] { 1, 2, 6, 7 };
+            var date = @DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss");
+            var mkd = (
+                from ttt in (
+                    from ggg in (
+                        from oba in registryContext.OwnershipBuildingsAssoc
+                        join owr in registryContext.OwnershipRights
+                            on oba.IdOwnershipRight equals owr.IdOwnershipRight
+                        where numbers.Contains(owr.IdOwnershipRightType)
+                        orderby oba.IdBuilding ascending, owr.Date descending
+                        select new { oba, owr }
+                    )
+                    group ggg by ggg.oba.IdBuilding into gr
+                    select new
+                    {
+                        oba = gr.Select(g => g.oba).FirstOrDefault(),
+                        owr = gr.Select(g => g.owr).FirstOrDefault()
+                    }
+                )
+                where ttt.owr.IdOwnershipRightType == 7
+                select ttt
+                );
+            //var premiseMun = (
+                
+            //    );
+            var countMKD = mkd.Count();
+            return Json(new { date, countMKD });
+        }
+    }
+}
---
+++ RegistryWeb/wwwroot/js/index.OwnerReports.js
@@ -0,0 +1,26 @@
+﻿$(function () {
+    $('.reportStatistic').hide();
+    $('.toggleReportStatistic').click(function (event) {
+        var id = $(this).data('id');
+        var reportStatistic = $('.reportStatistic').filter(function (i) {
+            return $(this).data('id') === id;
+        });
+        if (reportStatistic.is(":hidden")) {
+            console.log('развернуть');
+            $.ajax({
+                async: false,
+                type: 'POST',
+                url: window.location.origin + '/Statistic/ReestrStatistic',
+                success: function (data) {
+                    reportStatistic.find('.date_S').empty();
+                    reportStatistic.find('.countMKD_S').empty();
+                    console.log(data.date);
+                    console.log(data.countMKD);
+                    reportStatistic.find('.date_S').append(data.date);
+                    reportStatistic.find('.countMKD_S').append(data.countMKD);
+                }
+            });
+        }
+        reportStatistic.toggle();
+    });
+});
