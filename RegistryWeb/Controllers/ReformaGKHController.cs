554b5eeaafdf3ad7459ff0aa14d6d7f4f6e78ff7
zarav <zarkov.av@bratsk-city.ru>
2020-01-24 11:32:33 +0800

    Рефакторинг кода.

--- RegistryWeb/Controllers/ReformaGKHController.cs
+++ RegistryWeb/Controllers/ReformaGKHController.cs
@@ -1,18 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RegistryWeb.SecurityServices;
-using System.IO;
-using System.Net;
-using System.Text;
 using System.Xml.Linq;
 using System.Linq;
 using RegistryWeb.DataServices;
 using RegistryWeb.ViewModel;
 using System;
 using RegistryWeb.Models.Api;
-using System.Xml.Serialization;
-using System.Collections.Generic;
-using System.Runtime.Serialization;
-using System.Xml;
 using RegistryWeb.Models;
 
 namespace RegistryWeb.Controllers
@@ -20,17 +13,12 @@ namespace RegistryWeb.Controllers
     public class ReformaGKHController : Controller
     {
         private readonly ReformaGKHService reformaGKH;
-        private readonly SecurityService securityService;
-        private WebProxy proxy;
+        private readonly SecurityService securityService;   
 
         public ReformaGKHController(ReformaGKHService reformaGKH, SecurityService securityService)
         {
             this.reformaGKH = reformaGKH;
             this.securityService = securityService;
-            proxy = new WebProxy("http://proxy.mcs.br:8080/array.dll?Get.Routing.Script", false)
-            {
-                UseDefaultCredentials = true
-            };
         }
 
         public IActionResult Index()
@@ -41,60 +29,14 @@ namespace RegistryWeb.Controllers
             return View();
         }
 
-        private XDocument GetResponseSoap(string xmlString)
-        {
-            var request = WebRequest.Create(reformaGKH.ApiUrl);
-            request.Proxy = proxy;
-            request.Method = "POST";
-            request.ContentType = "application/xml";
-            byte[] byteArray = Encoding.UTF8.GetBytes(xmlString);
-            request.ContentLength = byteArray.Length;
-            var dataStream = request.GetRequestStream();
-            dataStream.Write(byteArray, 0, byteArray.Length);
-
-            var response = (HttpWebResponse)request.GetResponse();
-            var stream = response.GetResponseStream();
-            var reader = new StreamReader(stream, Encoding.UTF8);
-            var xDoc = XDocument.Load(reader);
-            response.Close();
-
-            return xDoc;
-        }
-
-        public T Deserialize<T>(XDocument xDoc)
-        {
-            var serializer = new XmlSerializer(typeof(T));
-            var reader = xDoc.CreateReader();
-            T result = (T)serializer.Deserialize(reader);
-            return result;
-        }
-
-        public T Deserialize<T>(string str)
-        {
-            var xDoc = XDocument.Parse(str);
-            return Deserialize<T>(xDoc);
-        }
-
-        public XDocument Serialize<T>(T serializeObject)
-        {
-            var serializer = new XmlSerializer(typeof(T));
-            var xDoc = new XDocument();
-            using (var writer = xDoc.CreateWriter())
-            {
-                serializer.Serialize(writer, serializeObject);
-            }
-            return xDoc;
-        }
-
-
         public IActionResult Test()
         {
             try
             {
                 var entity1 = ApiTest.GetHouseProfileActualResult();
-                var xDoc = Serialize<HouseProfileActualResult>(entity1);
+                var xDoc = reformaGKH.Serialize<HouseProfileActualResult>(entity1);
                 xDoc.Save(@"D:\houseProfileActualResult.xml");
-                var entity2 = Deserialize<HouseProfileActualResult>(xDoc);
+                var entity2 = reformaGKH.Deserialize<HouseProfileActualResult>(xDoc);
             }
             catch (Exception ex)
             {
@@ -113,7 +55,7 @@ namespace RegistryWeb.Controllers
                     TokenApiStorage.User = model.User;
                     TokenApiStorage.Password = model.Password;
                     var data = reformaGKH.Login(model.User, model.Password);
-                    var xDoc = GetResponseSoap(data);
+                    var xDoc = reformaGKH.GetResponseSoap(data);
                     TokenApiStorage.SessionGuid = xDoc.Descendants("LoginResult").Single().Value;
                 }
                 catch (Exception ex)
@@ -130,10 +72,10 @@ namespace RegistryWeb.Controllers
             try
             {
                 var data = reformaGKH.GetReportingPeriodList(TokenApiStorage.SessionGuid);
-                var xDoc = GetResponseSoap(data);
+                var xDoc = reformaGKH.GetResponseSoap(data);
                 var list = xDoc.Descendants("GetReportingPeriodListResult").SingleOrDefault();
                 xDoc = new XDocument(new XElement("PeriodListResult", list));
-                var pr = Deserialize<PeriodListResult>(xDoc);
+                var pr = reformaGKH.Deserialize<PeriodListResult>(xDoc);
             }
             catch (Exception ex)
             {
@@ -148,12 +90,12 @@ namespace RegistryWeb.Controllers
             try
             {
                 var data = reformaGKH.GetHouseProfileActual(TokenApiStorage.SessionGuid, 7947873, 465);
-                var xDoc = GetResponseSoap(data);
+                var xDoc = reformaGKH.GetResponseSoap(data);
                 var list = xDoc.Descendants("GetHouseProfileActualResult").SingleOrDefault();
                 xDoc = new XDocument(new XElement("HouseProfileActualResult",
                     new XAttribute(XNamespace.Xmlns + "xsi", list.GetNamespaceOfPrefix("xsi")),
                     list.Elements()));
-                var entity = Deserialize<HouseProfileActualResult>(xDoc);
+                var entity = reformaGKH.Deserialize<HouseProfileActualResult>(xDoc);
             }
             catch (Exception ex)
             {
--- RegistryWeb/DataServices/ReformaGKHService.cs
+++ RegistryWeb/DataServices/ReformaGKHService.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.Extensions.Configuration;
 using RegistryWeb.Models;
+using System.IO;
+using System.Net;
+using System.Text;
 using System.Xml.Linq;
+using System.Xml.Serialization;
 
 namespace RegistryWeb.DataServices
 {
@@ -10,6 +14,7 @@ namespace RegistryWeb.DataServices
         private XNamespace api;
         private readonly RegistryContext registryContext;
         public string ApiUrl { get; private set; }
+        private WebProxy proxy;
 
         public ReformaGKHService(RegistryContext registryContext, IConfiguration config)
         {
@@ -17,6 +22,55 @@ namespace RegistryWeb.DataServices
             ApiUrl = config.GetValue<string>("ApiUrl");
             soapenv = "http://schemas.xmlsoap.org/soap/envelope/";
             api = ApiUrl;
+            proxy = new WebProxy("http://proxy.mcs.br:8080/array.dll?Get.Routing.Script", false)
+            {
+                UseDefaultCredentials = true
+            };
+        }
+
+        public XDocument GetResponseSoap(string xmlString)
+        {
+            var request = WebRequest.Create(ApiUrl);
+            request.Proxy = proxy;
+            request.Method = "POST";
+            request.ContentType = "application/xml";
+            byte[] byteArray = Encoding.UTF8.GetBytes(xmlString);
+            request.ContentLength = byteArray.Length;
+            var dataStream = request.GetRequestStream();
+            dataStream.Write(byteArray, 0, byteArray.Length);
+
+            var response = (HttpWebResponse)request.GetResponse();
+            var stream = response.GetResponseStream();
+            var reader = new StreamReader(stream, Encoding.UTF8);
+            var xDoc = XDocument.Load(reader);
+            response.Close();
+
+            return xDoc;
+        }
+
+        public T Deserialize<T>(XDocument xDoc)
+        {
+            var serializer = new XmlSerializer(typeof(T));
+            var reader = xDoc.CreateReader();
+            T result = (T)serializer.Deserialize(reader);
+            return result;
+        }
+
+        public T Deserialize<T>(string str)
+        {
+            var xDoc = XDocument.Parse(str);
+            return Deserialize<T>(xDoc);
+        }
+
+        public XDocument Serialize<T>(T serializeObject)
+        {
+            var serializer = new XmlSerializer(typeof(T));
+            var xDoc = new XDocument();
+            using (var writer = xDoc.CreateWriter())
+            {
+                serializer.Serialize(writer, serializeObject);
+            }
+            return xDoc;
         }
 
         public string Login(string login, string password)
