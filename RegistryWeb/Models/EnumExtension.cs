939734712d0b68072d987f0919c13c0a84b94083
zarav <zarkov.av@bratsk-city.ru>
2020-02-03 11:23:46 +0800

    Небольшие правки в представления. Разбиение на несколько файлов главной страницы отчета.

---
+++ RegistryWeb/Models/EnumExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistryWeb.Models
+{
+    public static class EnumExtension
+    {
+        public static string GetRuName(this ProcessOwnershipTypeEnum en)
+        {
+            if (en == ProcessOwnershipTypeEnum.Private)
+                return "Частная";
+            return "Муниципальная";
+        }
+    }
+}
