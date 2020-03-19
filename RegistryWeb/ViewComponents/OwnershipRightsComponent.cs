29efc0a5599fbbfe220ca5ca0713cf78ed693b5a
merva <merkusheva.va@bratsk-city.ru>
2020-03-19 11:46:28 +0800

    Доработан компонент отображения ограничений. Учтена ситуация отображения принадлежности при заходе в помещение.

--- RegistryWeb/ViewComponents/OwnershipRightsComponent.cs
+++ RegistryWeb/ViewComponents/OwnershipRightsComponent.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using RegistryWeb.Models;
 using RegistryWeb.Models.Entities;
 using Microsoft.EntityFrameworkCore;
+using RegistryWeb.ViewModel;
 
 namespace RegistryWeb.ViewComponents
 {
@@ -19,33 +20,55 @@ namespace RegistryWeb.ViewComponents
 
         public IViewComponentResult Invoke(int id, AddressTypes type, string action)
         {
-            ViewBag.Action = action;
-            IQueryable<OwnershipRight> model = null;
+            IEnumerable<OwnershipRightVM> model = null;
             if (type == AddressTypes.Building)
+            {
                 model = GetBuildingOwnershipRights(id);
+            }
             if (type == AddressTypes.Premise)
+            {
                 model = GetPremiseOwnershipRights(id);
+            }
+            ViewBag.Type = type;
+            ViewBag.Action = action;
             return View("OwnershipRights", model);
         }
 
-        private IQueryable<OwnershipRight> GetBuildingOwnershipRights(int idBuilding)
+        private IEnumerable<OwnershipRightVM> GetBuildingOwnershipRights(int idBuilding)
         {
-            return registryContext.OwnershipBuildingsAssoc
+            var owrs = registryContext.OwnershipBuildingsAssoc
+                    .Include(oba => oba.OwnershipRightNavigation)
+                    .Where(oba => oba.IdBuilding == idBuilding)
+                    .Select(oba => oba.OwnershipRightNavigation)
+                    .Include(or => or.OwnershipRightTypeNavigation)
+                    .OrderBy(or => or.Date);
+            var r =
+                from owr in owrs
+                select new OwnershipRightVM(owr, AddressTypes.Building);
+            return r;
+        }
+
+        private IEnumerable<OwnershipRightVM> GetPremiseOwnershipRights(int idPremise)
+        {
+            var idBuilding = registryContext.Premises
+                .Include(p => p.IdBuildingNavigation)
+                .FirstOrDefault(p => p.IdPremises == idPremise)
+                .IdBuildingNavigation
+                .IdBuilding;
+            var owrs_b = registryContext.OwnershipBuildingsAssoc
                 .Include(oba => oba.OwnershipRightNavigation)
                 .Where(oba => oba.IdBuilding == idBuilding)
                 .Select(oba => oba.OwnershipRightNavigation)
-                .Include(or => or.OwnershipRightTypeNavigation)
-                .OrderBy(or => or.Date);
-        }
-
-        private IQueryable<OwnershipRight> GetPremiseOwnershipRights(int idPremise)
-        {
-            return registryContext.OwnershipPremisesAssoc
+                .Include(owr => owr.OwnershipRightTypeNavigation);
+            var r1 = owrs_b.Select(o => new OwnershipRightVM(o, AddressTypes.Building)).ToList();
+            var owrs_pr = registryContext.OwnershipPremisesAssoc
                 .Include(opa => opa.OwnershipRightNavigation)
                 .Where(opa => opa.IdPremises == idPremise)
                 .Select(opa => opa.OwnershipRightNavigation)
-                .Include(or => or.OwnershipRightTypeNavigation)
-                .OrderBy(or => or.Date);
+                .Include(or => or.OwnershipRightTypeNavigation);
+            var r2 = owrs_pr.Select(o => new OwnershipRightVM(o, AddressTypes.Premise)).ToList();
+            var r = r1.Union(r2).OrderBy(or => or.Date);
+            return r;
         }
     }
 }
---
+++ RegistryWeb/ViewModel/OwnershipRightVM.cs
@@ -0,0 +1,27 @@
+﻿using RegistryWeb.Models.Entities;
+using RegistryWeb.Models;
+namespace RegistryWeb.ViewModel
+{
+    public class OwnershipRightVM : OwnershipRight
+    {
+        public AddressTypes AddressType { get; set; }
+
+        public OwnershipRightVM() { }
+
+        public OwnershipRightVM(OwnershipRight owr, AddressTypes type)
+        {
+            IdOwnershipRight = owr.IdOwnershipRight;
+            IdOwnershipRightType = owr.IdOwnershipRight;
+            Number = owr.Number;
+            Date = owr.Date;
+            Description = owr.Description;
+            ResettlePlanDate = owr.ResettlePlanDate;
+            DemolishPlanDate = owr.DemolishPlanDate;
+            Deleted = owr.Deleted;
+            OwnershipRightTypeNavigation = owr.OwnershipRightTypeNavigation;
+            OwnershipBuildingsAssoc = owr.OwnershipBuildingsAssoc;
+            OwnershipPremisesAssoc = owr.OwnershipPremisesAssoc;
+            AddressType = type;
+        }
+    }
+}
