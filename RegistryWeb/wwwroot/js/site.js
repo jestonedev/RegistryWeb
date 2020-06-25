0e459eeeaf08ecaaaea9fc2c21ae7da5b3a9776f
zarav <zarkov.av@bratsk-city.ru>
2020-06-25 09:15:27 +0800

    коммит для слияния

--- RegistryWeb/wwwroot/js/site.js
+++ RegistryWeb/wwwroot/js/site.js
@@ -1,17 +1,19 @@
-﻿let guid = function () {
-    function s4() {
-        return Math.floor((1 + Math.random()) * 0x10000)
-            .toString(16)
-            .substring(1);
-    }
-    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-        s4() + '-' + s4() + s4() + s4();
-}
-if ($.validator !== undefined) {
+﻿if ($.validator !== undefined) {
     $.extend($.validator.methods, {
         number: function (value, element) {
             return this.optional(element) || /^-?(?:\d+|\d{1,3}(?:[\s\.,]\d{3})+)(?:[\.,]\d+)?$/.test(value);
+        },
+        range: function (value, element, param) {
+            return this.optional(element) || (Number(value.replace(",", ".")) >= param[0] && Number(value.replace(",", ".")) <= param[1]);
         }
+    });
+    $(document).ready(function () {
+        $("[data-val-number]").attr("data-val-number", "Введите числовое значение");
+        var form = $("form")
+            .removeData("validator")
+            .removeData("unobtrusiveValidation");
+        $.validator.unobtrusive.parse(form);
+        form.validate();
     });
 }
 //Формат вызова:
@@ -31,7 +33,26 @@ let elementToogle = function (event) {
     }
     event.data.toggle(!isDisplay);
     event.preventDefault();
-}
+};
+
+// Переделанная версия функции elementToogle. elementToogle оставлена, для обратной совместимости
+let elementToogleHide = function (event) {
+    let elementArrow = $(this);
+    let isDisplay = isExpandElemntArrow(elementArrow);
+    //свернуть
+    if (isDisplay) {
+        elementArrow.html('∨');
+        event.data.addClass("toggle-hide");
+    }
+    //развернуть
+    else {
+        elementArrow.html('∧');
+        event.data.removeClass("toggle-hide");
+    }
+    //event.data.toggle(!isDisplay);
+    event.preventDefault();
+};
+
 let arrowAnimation = function (elemntArrow) {
     //свернуть
     if (elemntArrow.html() === '∧') {
@@ -53,3 +74,10 @@ let isExpandElemntArrow = function (elemntArrow) {
 $(function () {
     $("input[type='number']").inputSpinner();
 });
+
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
