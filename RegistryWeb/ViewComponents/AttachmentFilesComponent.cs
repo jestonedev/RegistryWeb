37bed946e777b260c7a133608bacbcdbe3b7fa9b
IgnVV <ignvv@bratsk-city.ru>
2020-07-21 11:27:40 +0800

    Недостающие файлы по предыдущему коммиту

---
+++ RegistryWeb/ViewComponents/AttachmentFilesComponent.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using RegistryWeb.Models;
+using RegistryWeb.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using RegistryWeb.ViewModel;
+
+namespace RegistryWeb.ViewComponents
+{
+    public class AttachmentFilesComponent : ViewComponent
+    {
+        private RegistryContext registryContext;
+
+        public AttachmentFilesComponent(RegistryContext registryContext)
+        {
+            this.registryContext = registryContext;
+        }
+
+        public IViewComponentResult Invoke(int id, AddressTypes type, string action)
+        {
+            ViewBag.Id = id;
+            IEnumerable<AttachmentFileVM> model = null;
+            if (type == AddressTypes.Building)
+            {
+                model = GetBuildingAttachmentFiles(id);
+            }
+            if (type == AddressTypes.Premise)
+            {
+                model = GetPremiseAttachmentFiles(id);
+            }
+            ViewBag.AddressType = type;
+            ViewBag.Action = action;
+
+            return View("AttachmentFiles", model);
+        }
+
+        private IEnumerable<AttachmentFileVM> GetBuildingAttachmentFiles(int idBuilding)
+        {
+            var afs = registryContext.BuildingAttachmentFilesAssoc
+                    .Include(oba => oba.ObjectAttachmentFileNavigation)
+                    .Where(oba => oba.IdBuilding == idBuilding)
+                    .Select(oba => oba.ObjectAttachmentFileNavigation);
+            var r =
+                from af in afs
+                select new AttachmentFileVM(af, AddressTypes.Building);
+            return r;
+        }
+
+        private IEnumerable<AttachmentFileVM> GetPremiseAttachmentFiles(int idPremise)
+        {
+            return null;
+        }
+    }
+}
