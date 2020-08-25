0677752e34f7caa85f82ccaaf663241cde5d02c9
IgnVV <ignvv@bratsk-city.ru>
2020-08-25 11:06:43 +0800

    Рефакторинг общего кода фильтра по адресу, пагинации и сортировки. Доработка внешнего вида фильтра по адресу (скругление краев справа+скрипт автозаполнения поля). Добавлен переход с карточки лицевого счета на помещение/здание (или несколько помещений/зданий при наличии привязки к нескольким)

--- RegistryWeb/wwwroot/js/index.Claims.js
+++ RegistryWeb/wwwroot/js/index.Claims.js
@@ -1,59 +1,4 @@
-﻿var autocompleteFilterOptionsAddress = function () {
-    $('#FilterOptions_Address_Text').autocomplete({
-        source: function (request, response) {
-            $.ajax({
-                type: 'POST',
-                url: window.location.origin + '/Address/AutocompleteFilterOptionsAddress',
-                dataType: 'json',
-                data: { text: request.term },
-                success: function (data) {
-                    if (data !== "0" && data !== undefined) {
-                        $('input[name="FilterOptions.Address.AddressType"]').val(data.idAddressType);
-                        response($.map(data.autocompletePairs, function (pair) {
-                            return { label: pair.item2, value: pair.item2, id: pair.item1 };
-                        }));
-                    }
-                }
-            });
-        },
-        select: function (event, ui) {
-            $('input[name="FilterOptions.Address.Id"]').val(ui.item.id);
-            filterClearModal();
-            $("form.filterForm").submit();
-        },
-        delay: 300,
-        minLength: 3
-    });
-};
-
-var addressClear = function () {
-    $('input[name="FilterOptions.Address.AddressType"]').val("");
-    $('input[name="FilterOptions.Address.Id"]').val("");
-    $('#FilterOptions_Address_Text').val("");
-};
-
-var focusOutFilterOptionsAddress = function () {
-    if ($('input[name="FilterOptions.Address.Id"]').val() === "") {
-        addressClear();
-    }
-};
-
-var addressFilterClearBtnVisibility = function () {
-    if ($('input[name="FilterOptions.Address.Id"]').val() !== "") {
-        $('#FilterOptions_Address_Text').prop("disabled", true);
-        $('#addressFilterClearBtn').show();
-    }
-    else {
-        $('#addressFilterClearBtn').hide();
-    }
-};
-var addressFilterClear = function () {
-    addressClear();
-    $('#FilterOptions_Address_Text').prop("disabled", false);
-    $('#addressFilterClearBtn').hide();
-    $("form.filterForm").submit();
-};
-var searchModal = function () {
+﻿var searchModal = function () {
     addressClear();
     if ($("form.filterForm").valid()) {
         $(".c-arithmetic-op").next(".form-group").find("input[type='text']").each(function (idx, elem) {
@@ -99,10 +44,6 @@ var toggleAmountDetails = function (e) {
 };
 
 $(function () {
-    autocompleteFilterOptionsAddress();
-    addressFilterClearBtnVisibility();
-    $('#FilterOptions_Address_Text').focusout(focusOutFilterOptionsAddress);
-    $('#addressFilterClearBtn').click(addressFilterClear);
     $('#searchModalBtn').click(searchModal);
     $('#filterClearModalBtn').click(filterClearModal);
     $('#filterClearBtn').click(filterClear);
@@ -111,21 +52,6 @@ $(function () {
         e.preventDefault();
         var modal = $("#filterModal");
         modal.modal('show');
-    });
-
-    $("a.sort").click(function (e) {
-        $('input[name="OrderOptions.OrderField"]').val($(this).data("order-field"));
-        $('input[name="OrderOptions.OrderDirection"]').val($(this).data("order-direction"));
-        $('#FilterOptions_Address_Text').prop("disabled", false);
-        $("form.filterForm").submit();
-        e.preventDefault();
-    });
-
-    $('.page-link').click(function (e) {
-        $('input[name="PageOptions.CurrentPage"]').val($(this).data("page"));
-        $('#FilterOptions_Address_Text').prop("disabled", false);
-        $("form.filterForm").submit();
-        e.preventDefault();
     });
 
     $(".rr-claim-details").on("click", toggleClaimDetails);
--- RegistryWeb/wwwroot/js/index.common.js
+++ RegistryWeb/wwwroot/js/index.common.js
@@ -1,6 +1,16 @@
 ﻿$(function () {
-    $('.page-link').click(function () {
+    $("a.sort").click(function (e) {
+        $('input[name="OrderOptions.OrderField"]').val($(this).data("order-field"));
+        $('input[name="OrderOptions.OrderDirection"]').val($(this).data("order-direction"));
+        $('#FilterOptions_Address_Text').prop("disabled", false);
+        $("form.filterForm").submit();
+        e.preventDefault();
+    });
+    $('.page-link').off("click");
+    $('.page-link').click(function (e) {
         $('input[name="PageOptions.CurrentPage"]').val($(this).data("page"));
-        $("form.r-filter-form").submit();
-    });    
+        $('#FilterOptions_Address_Text').prop("disabled", false);
+        $("form.filterForm").submit();
+        e.preventDefault();
+    });  
 });
