bc650392957943b445ed8b19d24b8ddb0fbf4c4a
zarav <zarkov.av@bratsk-city.ru>
2020-09-02 10:35:59 +0800

    Добавлена модель для таблицы lawyer

---
+++ RegistryWeb/Models/IEntityTypeConfiguration/LawyerConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RegistryWeb.Models.Entities;
+
+namespace RegistryWeb.Models.IEntityTypeConfiguration
+{
+    public class LawyerConfiguration : IEntityTypeConfiguration<Lawyer>
+    {
+        private string nameDatebase;
+
+        public LawyerConfiguration(string nameDatebase)
+        {
+            this.nameDatebase = nameDatebase;
+        }
+
+        public void Configure(EntityTypeBuilder<Lawyer> builder)
+        {
+            builder.HasKey(e => e.IdLawyer);
+
+            builder.ToTable("lawyers", nameDatebase);
+
+            builder.Property(e => e.IdLawyer)
+                .HasColumnName("id_lawyer")
+                .HasColumnType("int(11)");
+
+            builder.Property(e => e.SNP)
+                .HasColumnName("snp")
+                .HasMaxLength(255)
+                .IsUnicode(false);
+
+            builder.Property(e => e.Post)
+                .HasColumnName("post")
+                .HasMaxLength(255)
+                .IsUnicode(false);
+        }
+    }
+}
