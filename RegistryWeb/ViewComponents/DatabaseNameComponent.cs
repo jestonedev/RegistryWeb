39b931f7d7f3cbe17e8ba7ef0b9048b8973761f6
zarav <zarkov.av@bratsk-city.ru>
2020-10-13 16:31:06 +0800

    Добавлен компонент отображающий имя БД в меню

---
+++ RegistryWeb/ViewComponents/DatabaseNameComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace RegistryWeb.ViewComponents
+{
+    public class DatabaseNameComponent : ViewComponent
+    {
+        private IConfiguration config;
+
+        public DatabaseNameComponent(IConfiguration config)
+        {
+            this.config = config;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.Database = config.GetValue<string>("Database");
+            return View("DatabaseName");
+        }
+    }
+}
