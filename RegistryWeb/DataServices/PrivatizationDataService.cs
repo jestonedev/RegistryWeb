362b8889d4756ad6e411e6787b239db9a9ca10c4
zarav <zarkov.av@bratsk-city.ru>
2021-02-17 16:27:57 +0800

    Начало работы над реестром по приватизации

---
+++ RegistryWeb/DataServices/PrivatizationDataService.cs
@@ -0,0 +1,20 @@
+﻿using RegistryWeb.Models;
+using RegistryWeb.ViewModel;
+using RegistryWeb.ViewOptions;
+using RegistryWeb.ViewOptions.Filter;
+
+namespace RegistryWeb.DataServices
+{
+    public class PrivatizationDataService : ListDataService<PrivatizationListVM, PrivatizationFilter>
+    {
+        public PrivatizationDataService(RegistryContext registryContext) : base(registryContext)
+        {
+        }
+
+        public override PrivatizationListVM InitializeViewModel(OrderOptions orderOptions, PageOptions pageOptions, PrivatizationFilter filterOptions)
+        {
+            var viewModel = base.InitializeViewModel(orderOptions, pageOptions, filterOptions);
+            return viewModel;
+        }
+    }
+}
