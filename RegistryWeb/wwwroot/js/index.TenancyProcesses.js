b535d27123ddd258fd732a518e4e843bbb41ade6
zarav <zarkov.av@bratsk-city.ru>
2021-03-02 14:21:00 +0800

    В фильтр найма добавлена возможность выбора "=", "≥" и "≤" для полей "Дата регистрации", "Дата выдачи", "Дата начала действия" и "Дата окончания". [#1790]

--- RegistryWeb/wwwroot/js/index.TenancyProcesses.js
+++ RegistryWeb/wwwroot/js/index.TenancyProcesses.js
@@ -5,6 +5,11 @@
 
 var filterClearModal = function () {
     $("#filterModal input[type='text'], #filterModal input[type='date'], #filterModal input[type='hidden'], #filterModal select").val("");
+    $("#FilterOptions_RegistrationDateSign, #FilterOptions_IssuedDateSign, #FilterOptions_BeginDateSign, #FilterOptions_EndDateSign")
+        .val("=")
+        .closest(".input-group")
+        .find("button.dropdown-toggle")
+        .text("=");
     $('#FilterOptions_IdStreet, #FilterOptions_IdPreset').selectpicker('render');
     $('#FilterOptions_IdsRentType').selectpicker('deselectAll');
     $('#FilterOptions_IdsObjectState').selectpicker("deselectAll");
@@ -42,13 +47,54 @@ var selectRegNumState = function (e) {
         inputRegNumIsEmpty.val("False");
     }
 };
+var selectRegistrationDateState = function (e) {
+    var value = $(this).text();
+    var wrapperDropDown = $(this).closest(".rr-registration-date-dropdown");
+    wrapperDropDown.find("button.dropdown-toggle").text(value);
+    wrapperDropDown
+        .closest(".input-group")
+        .find("#FilterOptions_RegistrationDateSign")
+        .val(value);
+};
+var selectIssuedDateState = function (e) {
+    var value = $(this).text();
+    var wrapperDropDown = $(this).closest(".rr-issued-date-dropdown");
+    wrapperDropDown.find("button.dropdown-toggle").text(value);
+    wrapperDropDown
+        .closest(".input-group")
+        .find("#FilterOptions_IssuedDateSign")
+        .val(value);
+};
+var selectBeginDateState = function (e) {
+    var value = $(this).text();
+    var wrapperDropDown = $(this).closest(".rr-begin-date-dropdown");
+    wrapperDropDown.find("button.dropdown-toggle").text(value);
+    wrapperDropDown
+        .closest(".input-group")
+        .find("#FilterOptions_BeginDateSign")
+        .val(value);
+};
+var selectEndDateState = function (e) {
+    var value = $(this).text();
+    var wrapperDropDown = $(this).closest(".rr-end-date-dropdown");
+    wrapperDropDown.find("button.dropdown-toggle").text(value);
+    wrapperDropDown
+        .closest(".input-group")
+        .find("#FilterOptions_EndDateSign")
+        .val(value);
+};
 
 $(function () {
     $('#searchModalBtn').click(searchModal);
     $('#filterClearModalBtn').click(filterClearModal);
     $('#filterClearBtn').click(filterClear);
     initRegNumState();
+
     $(".rr-registration-num-dropdown a.dropdown-item").click(selectRegNumState);
+    $(".rr-registration-date-dropdown a.dropdown-item").click(selectRegistrationDateState);
+    $(".rr-issued-date-dropdown a.dropdown-item").click(selectIssuedDateState);
+    $(".rr-begin-date-dropdown a.dropdown-item").click(selectBeginDateState);
+    $(".rr-end-date-dropdown a.dropdown-item").click(selectEndDateState);
 
     $("#filterModalShow").on("click", function (e) {
         e.preventDefault();
