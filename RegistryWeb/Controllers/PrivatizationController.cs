33b2fac511c18aef7622dd91acfe8a012c860075
zarav <zarkov.av@bratsk-city.ru>
2021-03-02 11:53:23 +0800

    *

--- RegistryWeb/Controllers/PrivatizationController.cs
+++ RegistryWeb/Controllers/PrivatizationController.cs
@@ -26,5 +26,125 @@ namespace RegistryWeb.Controllers
                 viewModel.PageOptions,
                 viewModel.FilterOptions));
         }
+
+        //public IActionResult Create()
+        //{
+        //    canEditBaseInfo = securityService.HasPrivilege(Privileges.RegistryWriteNotMunicipal) || securityService.HasPrivilege(Privileges.RegistryWriteMunicipal);
+        //    canEditDemolishingInfo = securityService.HasPrivilege(Privileges.RegistryWriteDemolishingInfo);
+        //    canAttachAdditionalFiles = securityService.HasPrivilege(Privileges.RegistryAttachAdditionalFiles);
+        //    if (!canEditBaseInfo)
+        //        return View("NotAccess");
+        //    return GetBuildingView(null);
+        //}
+
+        //[HttpPost]
+        //public IActionResult Create(Building building)
+        //{
+        //    if (building == null)
+        //        return NotFound();
+        //    canEditBaseInfo = securityService.HasPrivilege(Privileges.RegistryWriteNotMunicipal) || securityService.HasPrivilege(Privileges.RegistryWriteMunicipal);
+        //    canEditDemolishingInfo = securityService.HasPrivilege(Privileges.RegistryWriteDemolishingInfo);
+        //    canAttachAdditionalFiles = securityService.HasPrivilege(Privileges.RegistryAttachAdditionalFiles);
+        //    if (!canEditBaseInfo)
+        //        return View("NotAccess");
+        //    if (!canEditDemolishingInfo)
+        //    {
+        //        building.BuildingDemolitionActFiles = null;
+        //    }
+        //    if (!canAttachAdditionalFiles)
+        //    {
+        //        building.BuildingAttachmentFilesAssoc = null;
+        //    }
+        //    if (ModelState.IsValid)
+        //    {
+        //        dataService.Create(building, HttpContext.Request.Form.Files.Select(f => f).ToList());
+        //        return RedirectToAction("Details", new { building.IdBuilding });
+        //    }
+        //    return Error("Здание не было создано!");
+        //}
+
+        //public IActionResult Details(int? idContract, string returnUrl)
+        //{
+        //    ViewBag.ReturnUrl = returnUrl;
+        //    if (idBuilding == null)
+        //        return NotFound();
+        //    if (!securityService.HasPrivilege(Privileges.RegistryRead))
+        //        return View("NotAccess");
+        //    var building = dataService.GetBuilding(idBuilding.Value);
+        //    if (building == null)
+        //        return NotFound();
+        //    canEditBaseInfo = CanEditBuildingBaseInfo(building);
+        //    canEditDemolishingInfo = securityService.HasPrivilege(Privileges.RegistryWriteDemolishingInfo);
+        //    canAttachAdditionalFiles = securityService.HasPrivilege(Privileges.RegistryAttachAdditionalFiles);
+        //    return GetBuildingView(building);
+        //}
+
+        //[HttpGet]
+        //public IActionResult Delete(int? idBuilding, string returnUrl)
+        //{
+        //    ViewBag.ReturnUrl = returnUrl;
+        //    if (idBuilding == null)
+        //        return NotFound();
+        //    var building = dataService.GetBuilding(idBuilding.Value);
+        //    if (building == null)
+        //        return NotFound();
+        //    if (!CanEditBuildingBaseInfo(building))
+        //        return View("NotAccess");
+        //    canEditBaseInfo = false;
+        //    canEditDemolishingInfo = securityService.HasPrivilege(Privileges.RegistryWriteDemolishingInfo);
+        //    canAttachAdditionalFiles = securityService.HasPrivilege(Privileges.RegistryAttachAdditionalFiles);
+        //    return GetBuildingView(building);
+        //}
+
+        //[HttpPost]
+        //public IActionResult Delete(Building building)
+        //{
+        //    if (building == null)
+        //        return NotFound();
+        //    var b = dataService.GetBuilding(building.IdBuilding);
+        //    if (b == null)
+        //        return NotFound();
+        //    canEditBaseInfo = CanEditBuildingBaseInfo(b);
+        //    if (!canEditBaseInfo)
+        //        return View("NotAccess");
+        //    dataService.Delete(b.IdBuilding);
+        //    return RedirectToAction("Index");
+        //}
+
+        //[HttpGet]
+        //public IActionResult Edit(int? idBuilding, string returnUrl)
+        //{
+        //    ViewBag.ReturnUrl = returnUrl;
+        //    if (idBuilding == null)
+        //        return NotFound();
+        //    var building = dataService.GetBuilding(idBuilding.Value);
+        //    if (building == null)
+        //        return NotFound();
+        //    canEditBaseInfo = CanEditBuildingBaseInfo(building);
+        //    canEditDemolishingInfo = securityService.HasPrivilege(Privileges.RegistryWriteDemolishingInfo);
+        //    canAttachAdditionalFiles = securityService.HasPrivilege(Privileges.RegistryAttachAdditionalFiles);
+        //    if (!(canEditBaseInfo || canEditDemolishingInfo || canAttachAdditionalFiles))
+        //        return View("NotAccess");
+        //    return GetBuildingView(building);
+        //}
+
+        //[HttpPost]
+        //public IActionResult Edit(Building building, string returnUrl)
+        //{
+        //    ViewBag.ReturnUrl = returnUrl;
+        //    if (building == null)
+        //        return NotFound();
+        //    canEditBaseInfo = CanEditBuildingBaseInfo(building);
+        //    canEditDemolishingInfo = securityService.HasPrivilege(Privileges.RegistryWriteDemolishingInfo);
+        //    canAttachAdditionalFiles = securityService.HasPrivilege(Privileges.RegistryAttachAdditionalFiles);
+        //    if (!canEditBaseInfo)
+        //        return View("NotAccess");
+        //    if (ModelState.IsValid)
+        //    {
+        //        dataService.Edit(building);
+        //        return RedirectToAction("Details", new { building.IdBuilding });
+        //    }
+        //    return Error("Здание не было сохранено!");
+        //}
     }
 }
