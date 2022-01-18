2eaf637664f6c3a5411b7ebc04dbc738a3a0fb3e
merva <merkusheva.va@bratsk-city.ru>
2022-01-18 11:53:09 +0800

    Добавлен коэффициент для нового статуса объектов собственности "Расселено, но является муниципальным" (#1935).
    Signed-off-by: merva <merkusheva.va@bratsk-city.ru>

--- RegistryWeb/DataHelpers/ObjectStateHelper.cs
+++ RegistryWeb/DataHelpers/ObjectStateHelper.cs
@@ -9,7 +9,7 @@ namespace RegistryWeb.DataHelpers
     {
         public static List<int> MunicipalIds()
         {
-            return new List<int> { 4, 5, 9, 11, 12 };
+            return new List<int> { 4, 5, 9, 11, 12, 14 };
         }
 
         public static bool IsMunicipal(int idState)
